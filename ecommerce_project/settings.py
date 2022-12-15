b37c2d27e1c0e4c40d9512dd6c9ad5d7cdffcb4c
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-12-14 23:26:41 -0600

    set up django, need to add product pages and complete homepage

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ecommerce_project_app/'
+    'ecommerce_project_app'
 ]
 
 MIDDLEWARE = [

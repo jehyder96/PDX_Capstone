6ca4843d005b49d1c20104a2cffcda6875770c68
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-12-15 23:01:25 -0600

    views and urls are functional. need to add buttons to redirect to pages next

--- ecommerce_project_app/views.py
+++ ecommerce_project_app/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'pages/home.html')
 
-def about(request):
-    return render(request, 'pages/about.html')
+def cart(request):
+    return render(request, 'pages/cart.html')
+
+def checkout(request):
+    return render(request, 'pages/checkout.html')

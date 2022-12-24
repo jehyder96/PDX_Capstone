d78bcf89277c6a25353c420381af15490266f70a
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-12-23 23:23:01 -0600

    added project images, need to resize

--- ecommerce_project_app/models.py
+++ ecommerce_project_app/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     product_type = models.CharField(max_length=100, null=True)
     price = models.FloatField()
-    #image set this later
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name

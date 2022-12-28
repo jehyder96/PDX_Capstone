f497a77e7925b57db669eeef25909c87500c552e
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2022-12-28 09:19:42 -0600

    complete data structures, need to fix functionality

--- ecommerce_project_app/views.py
+++ ecommerce_project_app/views.py
@@ -15,6 +15,13 @@ def cart(request):
     return render(request, 'pages/cart.html')
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_total': 0}
     return render(request, 'pages/checkout.html')
 
 def about(request):

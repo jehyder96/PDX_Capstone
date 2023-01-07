45fc99fdaace47f96f767e5ffaeccce01360ce4c
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-01-06 23:24:49 -0600

    need to fix cart summary all items

--- PDX_Capstonev2/cart/cart.py
+++ PDX_Capstonev2/cart/cart.py
@@ -1,23 +1,43 @@
 from ecommerce_app.models import Product
+from decimal import Decimal
 
 class Cart():
 
     def __init__(self, request):
 #if someone has never been to our ecommerce store, this will create a session for them
-        self.session = request.session 
+        self.session = request.session
+        # returning user- obtain their existing session 
         cart = self.session.get('session_key')
-# new user - generate a new session
+        # new user - generate a new session
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
         
-        self.cart = {}
+        self.cart = cart
     
     def add(self, product, product_qty):
 
-        product_id = str(product.id)
+        product_id = str(product.id) #only want the user to change the product quantity
         if product_id in self.cart: #if product is in the cart, it can only modify the quantity
             self.cart[product_id]['qty'] = product_qty
         else:
             self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
 
-        self.session.modified = True
+        self.session.modified = True #telling Django that the session has been modified
+
+    #function to count all items in the cart class
+    def __len__(self):
+        return sum(item['qty'] for item in self.cart.values()) #the .values method gets the total items in shopping cart
+
+    #to iterate through all the products of the shopping cart
+
+    def __iter__(self):
+        all_products_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=all_products_ids)#to verify what is in the users cart
+        cart = self.cart.copy() #copy an instance of the session data
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total'] = item['price'] * item['qty']
+
+        yield item #pauses the execution but delivers the object
--- PDX_Capstonev2/cart/views.py
+++ PDX_Capstonev2/cart/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render
 from .cart import Cart
 from ecommerce_app.models import Product
 from django.shortcuts import get_object_or_404 #get a product object and if it doesn't exist, return error 404 not found
+from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, 'cart/cart-summary.html')
+
+    cart = Cart(request)
+
+    return render(request, 'cart/cart-summary.html', {'cart':cart})
 
 def cart_add(request):
 
     cart = Cart(request)
 
-    if request.POST.get('action') == 'POST':
+    if request.POST.get('action') == 'post':
+
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
         product = get_object_or_404(Product, id=product_id) #we want to get an object from the model product from the ajax request
         cart.add(product=product, product_qty=product_quantity)
+
+        cart_quantity = cart.__len__()
+
+        response = JsonResponse({'qty': cart_quantity})
+
+        return response
 
 def cart_delete(request):
 
